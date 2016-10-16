52d9ee6c4b62a62054d96d5ab15d2334db12c9b3
DoogeJ <Jaap-Willem.Dooge@outlook.com>
2016-10-16 06:32:38 +0200

    Initial commit

---
+++ NetPing/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NetPing")]
+[assembly: AssemblyDescription("NetPing by DoogeJ - https://github.com/DoogeJ/NetPing")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DoogeJ")]
+[assembly: AssemblyProduct("NetPing")]
+[assembly: AssemblyCopyright("Attribution 4.0 International (CC BY 4.0) - https://creativecommons.org/licenses/by/4.0/")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("629f6b40-c01f-4a0c-bc06-3be3b371b67f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
